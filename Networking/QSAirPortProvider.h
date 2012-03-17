8cea87abb50c16ec4416d0ae2320946324dd4237
Rob McBroom <github@skurfer.com>
2012-03-17 10:34:49 -0400

    bringing in files from the old AirPort plug-in

---
+++ Networking/QSAirPortProvider.h
@@ -0,0 +1,10 @@
+#define kQSWirelessNetworkType @"QSWirelessNetworkType"
+#define kQSAirPortItemType @"QSAirPortItemType"
+
+@interface QSAirPortNetworkObjectSource : QSObjectSource
+
+@end
+
+@interface QSAirPortNetworkActionProvider : QSActionProvider
+
+@end
